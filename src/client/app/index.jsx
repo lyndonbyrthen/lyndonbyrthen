c2a78ad6eb353709cf863984726634039e0ab87a
Unknown <lyndonbyrthen@gmail.com>
2017-07-30 22:11:30 -0400

    Begin with react afresh

---
+++ src/client/app/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import Game from './Game.jsx';
+import StackAPI from './StackAPI.jsx';
+
+
+ReactDOM.render(<StackAPI />, document.getElementById("root"));
