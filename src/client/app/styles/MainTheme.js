d332ce93dbb847cf43192b322dffa5527145742d
DESKTOP-4Q5UMP4\Lyndon Byrthen <lyndonbyrthen@gmail.com>
2017-08-14 12:14:28 -0400

    typography tweak

--- src/client/app/styles/MainTheme.js
+++ src/client/app/styles/MainTheme.js
@@ -2,14 +2,35 @@ import createMuiTheme from 'material-ui/styles/theme';
 import createPalette from 'material-ui/styles/palette';
 import cyan from 'material-ui/colors/cyan';
 import amber from 'material-ui/colors/amber';
+import createTypography from 'material-ui/styles/typography';
+import Typography from 'material-ui/Typography';
+
+let palette = createPalette({
+                primary: cyan,
+                accent:amber
+              })
+let typography = createTypography(palette, {
+                  // System font
+                  fontFamily:
+                    '"Roboto Slab",Roboto,"Helvetica Neue",Arial,sans-serif',
+                })
 
 const MainTheme = createMuiTheme({
 
-  palette: createPalette({
-    primary: cyan,
-    accent:amber
-  }),
-  
+  palette: palette,
+
+  // typography: typography,
+  typography: {
+
+      ...typography,
+     
+      body1: {
+        fontFamily:'Roboto,"Helvetica Neue",Arial,sans-serif',
+        color:'#333',
+        fontWeight:'300'
+      }
+  },
+
   overrides: {
 
     MuiDialog: {
